ce7d3c5a96da03ca462ed8c00225e43c889d6033
Jack Bendtsen <jackdbendtsen@gmail.com>
2022-04-22 12:27:22 +1000

    Added move_up() and move_down() to code-editor

--- client/code-editor.js
+++ client/code-editor.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// I'm sure there's a cleaner way to do this but don't care lmao
-function code_editor_blink(editor) {
-	editor.blink();
-}
-
 class CodeEditor {
 	constructor(element) {
 		this.text = "";
@@ -12,12 +7,15 @@ class CodeEditor {
 		this.cur2 = 0;
 		this.cursor_col = 0;
 		this.cursor_row = 0;
+		this.target_vis_col = -1;
+		this.prev_action = "";
 		this.blink_timer = null;
 		this.blink_state = true;
 
 		this.tab_w = 4;
 		this.fore = "#111";
 		this.back = "#eee";
+		this.hl   = "#48f";
 
 		this.elem = element;
 		this.canvas = element.getContext("2d");
@@ -39,12 +37,30 @@ class CodeEditor {
 		if (this.cur1 != this.cur2)
 			return;
 
-		this.blink_timer = setTimeout(code_editor_blink, 1000, this);
+		this.blink_timer = setTimeout(function(editor){editor.blink();}, 1000, this);
 
 		this.canvas.fillStyle = this.blink_state ? this.fore : this.back;
-		this.canvas.fillRect(this.cursor_col * this.char_w, (20 + this.char_base - this.char_h) + this.cursor_row * this.char_h, 2, this.char_h);
+		this.canvas.fillRect(this.cursor_col * this.char_w, this.cursor_row * this.char_h, 2, this.char_h);
 
 		this.blink_state = !this.blink_state;
+	}
+
+	find_target_column(start, end) {
+		var vis_cols = 0;
+		for (var i = start; i < end && i < this.text.length; i++) {
+			var code = this.text.charCodeAt(i);
+			if (code == 10)
+				break;
+
+			if (code == 9) {
+				vis_cols += this.tab_w - (vis_cols % this.tab_w);
+			}
+			else {
+				vis_cols++;
+			}
+		}
+
+		this.target_vis_cols = vis_cols;
 	}
 
 	move_distance(distance, selecting) {
@@ -58,8 +74,12 @@ class CodeEditor {
 	}
 
 	move_up(selecting) {
+		if (this.prev_action != "arrowup" && this.prev_action != "arrowdown")
+			this.target_vis_cols = -1;
+
 		var idx = this.cur2;
 		if (idx <= 0) {
+			this.target_vis_cols = -1;
 			this.cur2 = 0;
 			if (!selecting)
 				this.cur1 = this.cur2;
@@ -73,10 +93,150 @@ class CodeEditor {
 				break;
 			}
 		}
+
+		if (idx <= 0) {
+			this.cur2 = 0;
+			if (!selecting)
+				this.cur1 = this.cur2;
+			return;
+		}
+
+		var vis_cols = this.target_vis_cols;
+		if (vis_cols < 0) {
+			this.find_target_column(idx, this.cur2);
+			vis_cols = this.target_vis_cols;
+		}
+
+		idx--;
+		while (idx > 0) {
+			idx--;
+			if (this.text.charCodeAt(idx) == 10) {
+				idx++;
+				break;
+			}
+		}
+
+		var vis = 0;
+		while (vis < vis_cols) {
+			var code = this.text.charCodeAt(idx);
+			if (code == 10) {
+				break;
+			}
+			if (code == 9) {
+				vis += this.tab_w - (vis % this.tab_w);
+			}
+			else {
+				vis++;
+			}
+			idx++;
+		}
+
+		this.cur2 = idx;
+		if (!selecting)
+			this.cur1 = this.cur2;
+		return;
 	}
 
 	move_down(selecting) {
-		
+		if (this.prev_action != "arrowup" && this.prev_action != "arrowdown")
+			this.target_vis_cols = -1;
+
+		var len = this.text.length;
+		var idx = this.cur2;
+		if (idx >= len) {
+			this.target_vis_cols = -1;
+			this.cur2 = len;
+			if (!selecting)
+				this.cur1 = this.cur2;
+			return;
+		}
+
+		var end = idx;
+		while (end < len && this.text.charCodeAt(end) != 10) {
+			end++;
+		}
+
+		if (end >= len) {
+			this.cur2 = len;
+			if (!selecting)
+				this.cur1 = this.cur2;
+			return;
+		}
+
+		while (idx > 0) {
+			idx--;
+			if (this.text.charCodeAt(idx) == 10) {
+				idx++;
+				break;
+			}
+		}
+
+		var vis_cols = this.target_vis_cols;
+		if (vis_cols < 0) {
+			this.find_target_column(idx, this.cur2);
+			vis_cols = this.target_vis_cols;
+		}
+
+		idx = end + 1;
+		var vis = 0;
+		while (idx < len && vis < vis_cols) {
+			var code = this.text.charCodeAt(idx);
+			if (code == 10) {
+				break;
+			}
+			if (code == 9) {
+				vis += this.tab_w - (vis % this.tab_w);
+			}
+			else {
+				vis++;
+			}
+			idx++;
+		}
+
+		this.cur2 = idx;
+		if (!selecting)
+			this.cur1 = this.cur2;
+		return;
+	}
+
+	move_home(selecting) {
+		var len = this.text.length;
+		var idx = this.cur2;
+
+		var was_home = true;
+		while (idx > 0) {
+			idx--;
+			if (this.text.charCodeAt(idx) == 10) {
+				idx++;
+				break;
+			}
+			was_home = false;
+		}
+
+		if (was_home) {
+			while (idx < len && this.text.charCodeAt(idx) == 9) {
+				idx++;
+			}
+		}
+
+		this.cur2 = idx;
+		if (!selecting)
+			this.cur1 = this.cur2;
+		return;
+	}
+
+	move_end(selecting) {
+		var len = this.text.length;
+		var idx = this.cur2;
+
+		while (idx < len && this.text.charCodeAt(idx) != 10) {
+			idx++;
+		}
+
+		this.cur2 = idx;
+		if (!selecting)
+			this.cur1 = this.cur2;
+		return;
 	}
 
 	clear_selection() {
@@ -117,10 +277,139 @@ class CodeEditor {
 		this.cur2 = this.cur1;
 	}
 
+	indent(forwards) {
+		if (this.cur1 == this.cur2) {
+			this.insert("\t");
+			return;
+		}
+
+		var start = this.cur1 < this.cur2  ? this.cur1 : this.cur2;
+		var end   = this.cur1 >= this.cur2 ? this.cur1 : this.cur2;
+
+		var lines = 0;
+		for (var i = start; i < end + lines && i < this.text.length; i++) {
+			var c = this.text.charCodeAt(i);
+			if (c == 10) {
+				lines++;
+
+				var next = 0;
+				if (i < this.text.length-1)
+					next = this.text.charCodeAt(i+1);
+
+				if (next != 10) {
+					if (forwards) {
+						this.text = this.text.substring(0, i+1) + "\t" + this.text.substring(i+1);
+					}
+					else if (next == 9) {
+						this.text = this.text.substring(0, i+1) + this.text.substring(i+2);
+					}
+				}
+			}
+		}
+
+		if (lines == 0) {
+			this.insert("\t");
+			return;
+		}
+
+		while (start > 0 && this.text.charCodeAt(start) != 10) {
+			start--;
+		}
+
+		if (forwards) {
+			this.text = this.text.substring(0, start) + "\t" + this.text.substring(start);
+		}
+		else if (this.text.charCodeAt(start) == 9) {
+			this.text = this.text.substring(0, start) + this.text.substring(start+1);
+		}
+
+		var start_diff = forwards ? 1 : -1;
+		var end_diff = lines + 1;
+		end_diff *= start_diff;
+
+		if (this.cur1 < this.cur2) {
+			this.cur1 += start_diff;
+			this.cur2 += end_diff;
+		}
+		else {
+			this.cur1 += end_diff;
+			this.cur2 += start_diff;
+		}
+	}
+
+	handle_keypress(action, ctrl, shift) {
+		var block_default = true;
+
+		if (action.length == 1) {
+			if (ctrl) {
+				if (action == "r") {
+					block_default = false;
+				}
+			}
+			else {
+				this.insert(action);
+			}
+		}
+		else {
+			action = action.toLowerCase();
+			console.log(action);
+			if (action == "control" || action == "shift" || action == "capslock") {
+				action = null;
+			}
+			else if (action == "enter") {
+				this.insert("\n");
+			}
+			else if (action == "tab") {
+				this.indent(!shift);
+			}
+			else if (action == "backspace") {
+				this.delete(-1);
+			}
+			else if (action == "delete") {
+				this.delete(0);
+			}
+			else if (action == "home") {
+				this.move_home(shift);
+			}
+			else if (action == "end") {
+				this.move_end(shift);
+			}
+			else if (action == "arrowleft") {
+				if (ctrl) {
+					
+				}
+				else {
+					this.move_distance(-1, shift);
+				}
+			}
+			else if (action == "arrowright") {
+				if (ctrl) {
+					
+				}
+				else {
+					this.move_distance(1, shift);
+				}
+			}
+			else if (action == "arrowup") {
+				this.move_up(shift);
+			}
+			else if (action == "arrowdown") {
+				this.move_down(shift);
+			}
+		}
+		if (action)
+			this.prev_action = action;
+
+		return block_default;
+	}
+
 	refresh() {
 		this.canvas.fillStyle = this.back;
 		this.canvas.fillRect(0, 0, this.elem.width, this.elem.height);
 		this.canvas.fillStyle = this.fore;
+
+		var start_sel = this.cur1 <  this.cur2 ? this.cur1 : this.cur2;
+		var end_sel   = this.cur1 >= this.cur2 ? this.cur1 : this.cur2;
 
 		var col = 0;
 		var row = 0;
@@ -131,19 +420,27 @@ class CodeEditor {
 			var test_col = col;
 
 			for (var j = i; j < len; j++) {
-				code = this.text.charCodeAt(j);
 				if (j == this.cur1) {
 					this.cursor_col = test_col;
 					this.cursor_row = row;
 				}
+
+				code = this.text.charCodeAt(j);
 				if (code < 0x20)
 					break;
+
+				if (j >= start_sel && j < end_sel) {
+					this.canvas.fillStyle = this.hl;
+					this.canvas.fillRect(test_col * this.char_w, row * this.char_h, this.char_w + 1, this.char_h + 1);
+				}
 
 				test_col++;
 			}
 
-			if (j > i)
-				this.canvas.fillText(this.text.substring(i, j), 1 + col * this.char_w, 20 + row * this.char_h);
+			if (j > i) {
+				this.canvas.fillStyle = this.fore;
+				this.canvas.fillText(this.text.substring(i, j), 1 + col * this.char_w, (row+1) * this.char_h - this.char_base);
+			}
 
 			col = test_col;
 
@@ -173,44 +470,8 @@ class CodeEditor {
 
 function code_editor_keydown_handler(event) {
 	var editor = event.target.code_editor;
-
-	if (event.ctrlKey) {
-		
-	}
-	else {
-		if (event.key.length == 1) {
-			editor.insert(event.key);
-		}
-		else {
-			var code = event.key.toLowerCase();
-			console.log(code);
-			if (code == "enter") {
-				editor.insert("\n");
-			}
-			else if (code == "tab") {
-				editor.insert("\t");
-				event.preventDefault();
-			}
-			else if (code == "backspace") {
-				editor.delete(-1);
-			}
-			else if (code == "delete") {
-				editor.delete(0);
-			}
-			else if (code == "arrowleft") {
-				editor.move_distance(-1, event.shiftKey);
-			}
-			else if (code == "arrowright") {
-				editor.move_distance(1, event.shiftKey);
-			}
-			else if (code == "arrowup") {
-				editor.move_up(event.shiftKey);
-			}
-			else if (code == "arrowdown") {
-				editor.move_down(event.shiftKey);
-			}
-		}
-	}
+	if (editor.handle_keypress(event.key, event.ctrlKey, event.shiftKey))
+		event.preventDefault();
 
 	editor.refresh();
 }
